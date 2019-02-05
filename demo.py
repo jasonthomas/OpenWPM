a8e694aac6522ab86aa2d82f83c5fd6f8d41c99d
Jason Thomas <jason@lithiumfox.com>
2019-02-05 16:44:57 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,29 +1,64 @@
 from __future__ import absolute_import
+import boto3
+import random
+import os
 
 from six.moves import range
 
 from automation import CommandSequence, TaskManager
 
-# The list of sites that we wish to crawl
-NUM_BROWSERS = 3
-sites = ['http://www.example.com',
-         'http://www.princeton.edu',
-         'http://citp.princeton.edu/']
+from io import BytesIO
+
+NUM_BROWSERS = 1
+
+S3_BUCKET = os.getenv('S3_BUCKET')
+S3_CRAWL_QUEUE = os.getenv('S3_CRAWL_QUEUE')
+S3_DIRECTORY = os.getenv('S3_DIRECTORY')
+
+# Get lists of sites from files in S3
+s3 = boto3.client('s3', region_name='us-east-1')
+bucket_name = S3_BUCKET
+
+paginator = s3.get_paginator('list_objects_v2')
+page_iterator = paginator.paginate(Bucket=bucket_name,
+                                   Prefix=S3_CRAWL_QUEUE)
+
+keys = []
+for page in page_iterator:
+    for content in page['Contents']:
+        keys.append(content['Key'])
+
+key = keys[random.randrange(len(keys))]
+print key
+
+obj = s3.get_object(Bucket=bucket_name, Key=key)
+
+sites = []
+
+for url in BytesIO(obj['Body'].read()):
+    sites.append(url.strip())
+
+s3.delete_object(Bucket=bucket_name, Key=key)
 
 # Loads the manager preference and 3 copies of the default browser dictionaries
 manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
 
 # Update browser configuration (use this for per-browser settings)
 for i in range(NUM_BROWSERS):
-    # Record HTTP Requests and Responses
+    browser_params[i]['cookie_instrument'] = True
+    browser_params[i]['js_instrument'] = True
     browser_params[i]['http_instrument'] = True
-    # Enable flash for all three browsers
-    browser_params[i]['disable_flash'] = False
-browser_params[0]['headless'] = True  # Launch only browser 0 headless
+    browser_params[i]['headless'] = True
+    browser_params[i]['save_javascript'] = True
+
 
 # Update TaskManager configuration (use this for crawl-wide settings)
 manager_params['data_directory'] = '~/Desktop/'
 manager_params['log_directory'] = '~/Desktop/'
+
+manager_params['output_format'] = 's3'
+manager_params['s3_bucket'] = S3_BUCKET
+manager_params['s3_directory'] = S3_DIRECTORY
 
 # Instantiates the measurement platform
 # Commands time out by default after 60 seconds
@@ -31,16 +66,8 @@ manager = TaskManager.TaskManager(manager_params, browser_params)
 
 # Visits the sites with all browsers simultaneously
 for site in sites:
-    command_sequence = CommandSequence.CommandSequence(site)
-
-    # Start by visiting the page
-    command_sequence.get(sleep=0, timeout=60)
-
-    # dump_profile_cookies/dump_flash_cookies closes the current tab.
-    command_sequence.dump_profile_cookies(120)
-
-    # index='**' synchronizes visits between the three browsers
-    manager.execute_command_sequence(command_sequence, index='**')
-
+    command_sequence = CommandSequence.CommandSequence(site, reset=True)
+    command_sequence.get(sleep=10, timeout=60)
+    manager.execute_command_sequence(command_sequence)
 # Shuts down the browsers and waits for the data to finish logging
 manager.close()
